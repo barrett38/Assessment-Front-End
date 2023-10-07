a2d764b0815d2ba73273344e54f263a651e311cd
Barrett Anderson <barrett1614@gmail.com>
2023-10-07 11:11:22 -0500

    Shortened JS files

--- about.js
+++ about.js
@@ -1,20 +1,16 @@
-const consoleLog = console.log("hello world");
-
-// PNG image of Purrloin
-const catImage = document.querySelector('img[src="./purrloin.png"]');
-
-// querySelector referring to home.html
-const form = document.querySelector('#contact');
+// console.log(`hello world`);
+// See https://ed.devmountain.com/materials/fah11/exercises/assessment-front-end-web/
 
 // Functions to handle form submission and mouseover events
-function handleSubmit(evt) {
-	evt.preventDefault();
-	alert('Form has been submitted successfully!');
-}
+const sub = `Form has been submitted successfully!`;
+const handleSubmit = (evt) => {alert(sub), evt.preventDefault()};
 
-function handleMouseOver(evt) {
-	alert('You have great taste in pictures!');
-}
+const pic = `Nice picture!`;
+const handleMouseOver = (evt) => {alert(pic);};
 
-form.addEventListener('submit', handleSubmit);
-catImage.addEventListener('mouseover', handleMouseOver);
+// Finding elements in the DOM and adding event listeners
+const form = document.querySelector(`#contact`);
+form.addEventListener(`submit`, handleSubmit);
+
+const catImage = document.querySelector(`img[src="./purrloin.png"]`);
+catImage.addEventListener(`mouseover`, handleMouseOver);
--- profile.js
+++ profile.js
@@ -1,19 +1,18 @@
+// I try to make my code as condensed as possible
+// That is the biggest thing I try to work on
+// See https://ed.devmountain.com/materials/fah11/exercises/assessment-front-end-web/
 
-// Section 1: Define buttons on site
+// Section 1: Define buttons and messages
 const button1 = document.getElementById('color');
+const fc = `Emerald Green`;
 const button2 = document.getElementById('place');
+const fp = `Anywhere with my family`;
 const button3 = document.getElementById('ritual');
+const fr = `Reading a good book.`;
+const msgs = {'color': fc, 'place': fp, 'ritual': fr};
 
-// Section 2: Define messages for each button
-function handleButtonClick(event) {
-    const messages = {
-        'color': 'Emerald Green',
-        'place': 'Anywhere with my family',
-        'ritual': 'Reading a good book.'
-    };
-    const message = messages[event.target.id] || na;
-    alert(message);
-}
+// Section 2: Define messages for each button with function
+const buttonClick = (ev) => alert(msgs[ev.target.id] || na);
 
 // Section 3: Define function to handle button click
-document.body.addEventListener('click', handleButtonClick);
+document.body.addEventListener('click', buttonClick);
